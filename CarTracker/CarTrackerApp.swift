69d67951f8622ba320b47f9d3b8239151bcd67a7
Hector Carmona <orcabe86@gmail.com>
2024-04-30 09:39:25 -0600

    Initial Commit

---
+++ CarTracker/CarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTrackerApp.swift
+//  CarTracker
+//
+//  Created by Hector Carmona on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
